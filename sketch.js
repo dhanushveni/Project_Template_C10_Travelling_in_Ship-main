f42869cb7fa0dbb95e894474d1398c5ae2403714
dhanushveni <88822254+dhanushveni@users.noreply.github.com>
2021-08-19 11:46:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var seaImg;
+var shipImg1;
+
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  seaImg.addImage(sea.png);
+  
+}
+
+//code to reset the background
+if(sea.x < 0){
+  sea.x = sea.width/2;
+
+function draw() {}
+  background("blue");
+  drawsprites();
+}
